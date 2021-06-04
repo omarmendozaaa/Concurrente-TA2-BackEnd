d1a5bbba1d78b6e1b9f96307a75ead9bbb0b52ad
omarmendozaaa <mendozavallejosomar@gmail.com>
2021-06-03 22:13:11 -0500

    update cors

--- server/api.go
+++ server/api.go
@@ -13,10 +13,29 @@ type Server interface {
 	Router() http.Handler
 }
 
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, req *http.Request) {
+			// Just put some headers to allow CORS...
+			w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+			w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+			// and call next handler!
+			next.ServeHTTP(w, req)
+		})
+}
+func enableCORS(router *mux.Router) {
+	router.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	}).Methods(http.MethodOptions)
+	router.Use(middlewareCors)
+}
 func New() Server {
 
 	a := &api{}
 	r := mux.NewRouter()
+	enableCORS(r)
 	//Empty Database
 	alumnos = append(alumnos, Alumno{ID: "1", Nombre: "Omar Mendoza", DNI: 33429504, Edad: 80})
 	alumnos = append(alumnos, Alumno{ID: "2", Nombre: "Roman Ramirez", DNI: 72511063, Edad: 57})
